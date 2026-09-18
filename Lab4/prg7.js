db7af2d046ddbd35225ca11bafbadb516d312224
aditya verma <piyushxvrm05@gmail.com>
2026-09-18 09:08:38 +0530

    users.js

--- Lab4/prg7.js
+++ Lab4/prg7.js
@@ -1,26 +1,46 @@
-import http from 'http';
-
+import http from "http";
+import {addUser, getUsers} from "./users.js"
 const server = http.createServer((req, res) => {
-    if(req.url === '/api/users', req.method == 'GET') {
-        res.end(JSON.stringify({ msg: "all users"}))
-    }
-    else if(req.url === '/api/users', req.method == 'POST') {
-        res.end(JSON.stringify({ msg: "add user"}))
-    }
-    else if(req.url === 'api/users/1', req.method === 'GET') {
-        res.end(JSON.stringify({ msg: "single user with id 1"})) 
-    }
-    else if(req.url === 'api/users/1', req.method === 'PUT') {
-        res.end(JSON.stringify({ msg: "update user 1"})) 
-    }
-    else if(req.url === 'api/users/1', req.method === 'DELETE') {
-        res.end(JSON.stringify({ msg: "remove 1"})) 
-    } else {
-        res.statusCode = 404;
-        res.end()
-    }
-});
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+      <html>
+        <head>
+          <title>My Node Server</title>
+        </head>
+        <body>
+          <h1>Hello from Node.js!</h1>
+          <p>My server is working successfully.</p>
+          <p>This page is being served from port 2000.</p>
+        </body>
+      </html>
+    `);
+  }
+  else if (req.url === "/api/users" && req.method === "GET") {
+    const userId = Number(req.url.split("/").pop());
+    res.end(JSON.stringify({msg: `Showing details of user with Id ${userId}`}));
+  } else if (req.url === "/api/users" && req.method === "POST") {
+    let body = "";
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      const user = JSON.parse(body);
+      const userCreated = addUser(user);
+      res.end(JSON.stringify({ msg: "user added", userCreated }));
+    });
 
+  } else if (req.url === "/api/users/2" && req.method === "GET")
+    res.end(JSON.stringify({ msg: "single user with id 1" }));
+  else if (req.url === "/api/users/1" && req.method == "PUT") {
+    res.end(JSON.stringify({ msg: "update user 1" }));
+  } else if (req.url === "/api/users/1" && req.method == "DELETE") {
+    res.end(JSON.stringify({ msg: "remove 1" }));
+  } else {
+    // res.statusCode = 404;
+    res.end();
+  }
+});
 server.listen(2000, () => {
-    console.log("Server is running on port 2000");
+  console.log("Server is running on port 2000");
 });
