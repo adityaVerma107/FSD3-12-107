3b1506754f26960d4a6f90245d9f57e9164097ed
aditya verma <piyushxvrm05@gmail.com>
2026-09-18 15:18:04 +0530

    updated

--- Lab1/eventEmitter.js
+++ Lab1/eventEmitter.js
@@ -1,25 +1,31 @@
 import { EventEmitter } from "node:events";
 
-const login=(name)=>{
- console.log(`${name} logged in`);
+const login = (name) => {
+  console.log(`${name} logged in`);
 };
 
-const start =() =>{
-    console.log("systemm starts");
+const start = () => {
+  console.log("System starts");
 };
-
-const working= (name)=>{
-    console.log(`${name} add items to cart`);
+const working = (name) => {
+  console.log(`${name} add items to cart`);
 };
-
-const checkout = (name) =>{
-    console.log(`${name}logged out`);
+const checkout = (name) => {
+  console.log(`${name} logged out`);
 };
 
 const task = new EventEmitter();
+task.once("greet", start);
 task.on("greet", login);
 task.on("greet", working);
+task.on("greet", checkout);
 
+task.once("exit", () => {
+  console.log("System shutting down");
+});
 
-task.emit("greet","Steve Rogers");
-task.emit("greet","Mudit Lohani");
+task.emit("greet", "Mayank Bansal");
+task.emit("greet", "Mudit Lohani");
+task.off("greet", working);
+task.emit("greet", "Manya Goyal");
+task.emit("exit", "Manager");
