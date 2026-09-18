04496231197ed41637dd6ad0dea08dc1bf4e0520
aditya verma <piyushxvrm05@gmail.com>
2026-09-18 15:29:12 +0530

    Lab3 completed

--- Lab3/prg2.js
+++ Lab3/prg2.js
@@ -2,7 +2,7 @@ import http from 'http'
 
 const server = http.createServer((req,res)=>{
     console.log("server hit by client ");
-    res.write("<h1>Hello Client </h1>");
+    res.write("<h1>Hello client </h1>");
     res.end();
 });
 
