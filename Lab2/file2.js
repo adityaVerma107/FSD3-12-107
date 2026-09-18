6cc63f4a1d40f90a5afcd7b813722a6bf6eb9509
aditya verma <piyushxvrm05@gmail.com>
2026-09-18 14:48:25 +0530

    /

--- Lab2/file2.js
+++ Lab2/file2.js
@@ -1,26 +1,38 @@
-import { readfFile } from "fs/promises";
+import { readFile, appendFile, writeFile, unlink } from "fs/promises";
 
+const readData = async (filename) => {
+  try {
+    const content = await readFile(filename, "utf-8");
+    return content;
+  } catch (e) {
+    console.log(e.message);
+    console.log("File not found");
+  } finally {
+    console.log("Read data finished");
+  }
+};
 
-const readData = async (filename)=>{
-try {
-    const content = await readFile(filename,`utf-8`);
-    return content; 
-} catch (error) {
-    console.error("Error reading file:", error);
-    throw error;
-}
-}
-const writeData = async (filename, content)=>{
-try {
+const writeData = async (filename, content) => {
+  try {
     await writeFile(filename, content);
-} catch (error) {
-    console.error("Error Writing in file:", error);
-    throw error;
-}
-}
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+const appendData = async (filename, content) => {
+  try {
+    await appendFile(filename, content);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+const deleteFile = async (filename) => {
+  try {
+    await unlink(filename);
+  } catch (error) {
+    console.log("File not found");
+  }
+};
 
-
-}
-const appendData = (filename, content)=> {
-
-}   
+const data = await readData("file1.js");
+console.log(data);
